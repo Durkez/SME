242a439e890c5092d75a2a016fe6378c51dc715c
2318294 <2318294@LTIN514778.cts.com>
2024-04-17 11:58:40 +0530

    HSME

---
+++ Hackathon_ policybazaar/src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,29 @@
+package testRunner;
+
+import org.junit.runner.RunWith;  
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+					features= {".//Features"},
+					//features= {".//Features/Travel.feature"},
+					//features= {".//Features/CarInsurance.feature"},
+					//features= {".//Features/HealthInsurance.feature"},
+					//features= {"@target/rerun.txt"},
+					glue="stepDefinitions",
+					plugin= {"pretty", "html:reports/myreport.html", 
+							  "rerun:target/rerun.txt",
+							  "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+							},
+							       
+					dryRun=false,    
+					monochrome=true,                
+					publish=false   
+		)
+public class TestRunner extends AbstractTestNGCucumberTests  {
+
+		} 
+ 
